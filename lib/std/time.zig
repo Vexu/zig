1be4cb8c799de80ffb45549e230e12ac805ead40
Vexu <15308111+Vexu@users.noreply.github.com>
2019-07-08 21:58:40 +0300

    added Date struct

--- lib/std/time.zig
+++ lib/std/time.zig
@@ -189,6 +189,179 @@ pub const Timer = struct {
     }
 };
 
+/// ISO 8601 compliant date representation.
+pub const Date = struct {
+    /// milliseconds, range 0 to 999
+    millisecond: u16,
+
+    /// seconds, range 0 to 60
+    second: u8,
+
+    /// minutes, range 0 to 59
+    minute: u8,
+
+    /// hours, range 0 to 23
+    hour: u8,
+
+    /// day of the month, range 1 to 31
+    day: u8,
+
+    /// day of the year, range 1 to 366
+    year_day: u16,
+
+    /// day of the week, enum Monday to Sunday
+    week_day: Day,
+
+    /// month, enum January to December
+    month: Month,
+
+    /// year, year 0000 is equal to 1 BCE
+    year: i32,
+
+    pub const Day = enum {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7,
+    };
+
+    pub const Month = enum {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12,
+    };
+
+    /// get current date
+    pub fn now() Date {
+        const time = milliTimestamp();
+        return fromTimestamp(time);
+    }
+
+    /// Converts milliTimestamp to date
+    pub fn fromTimestamp(time: u64) Date {
+        // Ported from musl, which is licensed under the MIT license:
+        // https://git.musl-libc.org/cgit/musl/tree/COPYRIGHT
+        const days_in_month = [12]i8{ 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 29 };
+        // 2000-03-01 (mod 400 year, immediately after feb29
+        const leapoch = 946684800 + 86400 * (31 + 29);
+        const days_per_400y = 365 * 400 + 97;
+        const days_per_100y = 365 * 100 + 24;
+        const days_per_4y = 365 * 4 + 1;
+
+        var date: Date = undefined;
+
+        date.millisecond = @truncate(u16, time % 1000);
+
+        var seconds = @intCast(i64, time / 1000) - leapoch;
+        var days = @divTrunc(seconds, 86400);
+        var rem_seconds = @truncate(i32, @rem(seconds, 86400));
+        if (rem_seconds < 0) {
+            rem_seconds += 86400;
+            days -= 1;
+        }
+
+        var week_day = @rem((3 + days), 7);
+        if (week_day < 0) {
+            week_day += 7;
+        }
+        date.week_day = @intToEnum(Day, @intCast(u3, week_day));
+
+        var qc_cycles = @divTrunc(days, days_per_400y);
+        var rem_days = @intCast(i32, @rem(days, days_per_400y));
+        if (rem_days < 0) {
+            rem_days += days_per_400y;
+            qc_cycles -= 1;
+        }
+
+        var c_cycles = @divTrunc(rem_days, days_per_100y);
+        if (c_cycles == 4) {
+            c_cycles -= 1;
+        }
+        rem_days -= c_cycles * days_per_100y;
+
+        var q_cycles = @divTrunc(rem_days, days_per_4y);
+        if (q_cycles == 25) {
+            q_cycles -= 1;
+        }
+        rem_days -= q_cycles * days_per_4y;
+
+        var rem_years = @divTrunc(rem_days, 365);
+        if (rem_years == 4) {
+            rem_years -= 1;
+        }
+        rem_days -= rem_years * 365;
+
+        var leap: i32 = if (rem_years == 0 and (q_cycles != 0 or c_cycles == 0)) 1 else 0;
+        var year_day = rem_days + 31 + 28 + leap;
+        if (year_day >= 365 + leap) {
+            year_day -= 365 + leap;
+        }
+
+        var years = rem_years + 4 * q_cycles + 100 * c_cycles + 400 * qc_cycles;
+
+        var months: i32 = 0;
+        while (days_in_month[@intCast(usize, months)] <= rem_days) : (months += 1) {
+            rem_days -= days_in_month[@intCast(usize, months)];
+        }
+
+        if (months >= 10) {
+            months -= 12;
+            years += 1;
+        }
+
+        date.year = @intCast(i32, years + 2000);
+        date.month = @intToEnum(Month, @intCast(u4, months + 3));
+        date.day = @intCast(u8, rem_days + 1);
+        date.year_day = @intCast(u16, year_day + 1);
+        date.hour = @intCast(u8, @divTrunc(rem_seconds, 3600));
+        date.minute = @intCast(u8, @rem(@divTrunc(rem_seconds, 60), 60));
+        date.second = @intCast(u8, @rem(rem_seconds, 60));
+        return date;
+    }
+
+    /// Converts date to milliTimestamp
+    pub fn toTimestamp(date: Date) !u64 {
+        if (date.year < 1970) {
+            return error.Invalid;
+        }
+
+        const after_epoch = @intCast(u64, date.year - 1970);
+        var leap_days = @divTrunc(after_epoch + 2, 4); // 1972 first leap year after 1970
+        leap_days -= (after_epoch % 400) * 3; // 400 years
+        leap_days -= (after_epoch % 100); // 100 years
+
+        var time: u64 = after_epoch * 365 + leap_days;
+
+        time += date.year_day - 1;
+
+        time *= 24;
+        time += date.hour;
+
+        time *= 60;
+        time += date.minute;
+
+        time *= 60;
+        time += @as(u64, date.second);
+
+        time *= 1000;
+        time += date.millisecond;
+
+        return time;
+    }
+};
+
 test "sleep" {
     sleep(1);
 }
@@ -218,4 +391,21 @@ test "Timer" {
 
     timer.reset();
     testing.expect(timer.read() < time_1);
+}
+
+test "Date" {
+    const date = Date.fromTimestamp(1560870105000);
+
+    testing.expect(date.millisecond == 0);
+    testing.expect(date.second == 45);
+    testing.expect(date.minute == 1);
+    testing.expect(date.hour == 15);
+    testing.expect(date.day == 18);
+    testing.expect(date.year_day == 169);
+    testing.expect(date.week_day == .Tuesday);
+    testing.expect(date.month == .June);
+    testing.expect(date.year == 2019);
+
+    const time = try date.toTimestamp();
+    testing.expect(time == 1560870105000);
 }
